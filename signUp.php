c492644ccd9682428de6868cf2d8ae38b4de9b90
amine1949 <amine.barik.2001@gmail.com>
2023-05-04 21:38:16 +0200

    Add my project to my repo

---
+++ signUp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Inclode Icon -->
+    <script src="https://kit.fontawesome.com/2f2d33a415.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="/image/glasses1.png" type="image/x-icon">
+    <!-- Inclode Icon -->
+    <title>GlassesVT Sign Up</title>
+</head>
+<body>
+    <div class="container">
+        <img src="/image/Logo.png" alt="Logo" class="logo">
+        <form action="/home.php">
+            <input type="text" name="Username" id="Username" placeholder="Username" >
+            <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
+            <input type="password" name="password" id="password" placeholder="Password">
+            <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
+            <input type="checkbox" name="rememberMe" id="rememberMe">
+            <span class="Rme">Remember me</span><br>
+            <input type="submit" value="Login">
+        </form>
+        <div class="test">
+            <span class="notHaveAnAccount">Not have an account ?</span>
+            <a href="/signUp.php">Sign up Here</a>         
+        </div>
+    </div>
+</body>
+</html>
