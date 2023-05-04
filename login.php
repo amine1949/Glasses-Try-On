f609b072ac33af2bf71f3bdb1c3b2d2758dc0139
amine1949 <amine.barik.2001@gmail.com>
2023-05-04 22:58:13 +0200

    Sign up not yet complited

--- login.php
+++ login.php
@@ -5,30 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Include css Login -->
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/Glasses-Try-On/css/style.css">
+    <link rel="stylesheet" href="/Glasses-Try-On/css/login.css">
     <!-- Include css Login -->
     <!-- Inclode Icon -->
     <script src="https://kit.fontawesome.com/2f2d33a415.js" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="/image/glasses1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/Glasses-Try-On/image/glasses1.png" type="image/x-icon">
     <!-- Inclode Icon -->
     <title>GlassesVT Login</title>
 </head>
 <body>
     <div class="container">
-        <img src="/image/Logo.png" alt="Logo" class="logo">
+        <img src="/Glasses-Try-On/image/Logo.png" alt="Logo" class="logo">
         <form action="/home.php">
-            <input type="text" name="Username" id="Username" placeholder="Username" >
-            <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
-            <input type="password" name="password" id="password" placeholder="Password">
-            <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
+            <div class="User">
+                <input type="text" name="Username" id="Username" placeholder="Username" required>
+                <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
+            </div>
+            <div class="pass">
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
+            </div>
             <input type="checkbox" name="rememberMe" id="rememberMe">
             <span class="Rme">Remember me</span><br>
             <input type="submit" value="Login">
         </form>
-        <div class="test">
+        <div class="signUpHere">
             <span class="notHaveAnAccount">Not have an account ?</span>
-            <a href="/signUp.php">Sign up Here</a>         
+            <a href="/Glasses-Try-On/signUp.php">Sign up Here</a>         
         </div>
     </div>
 </body>
--- signUp.php
+++ signUp.php
@@ -4,27 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include css Login -->
+    <link rel="stylesheet" href="/Glasses-Try-On/css/style.css">
+    <link rel="stylesheet" href="/Glasses-Try-On/css/signUp.css">
+    <!-- Include css Login -->
     <!-- Inclode Icon -->
     <script src="https://kit.fontawesome.com/2f2d33a415.js" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="/image/glasses1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/Glasses-Try-On/image/glasses1.png" type="image/x-icon">
     <!-- Inclode Icon -->
     <title>GlassesVT Sign Up</title>
 </head>
 <body>
     <div class="container">
-        <img src="/image/Logo.png" alt="Logo" class="logo">
+        <img src="/Glasses-Try-On/image/Logo.png" alt="Logo" class="logo">
         <form action="/home.php">
-            <input type="text" name="Username" id="Username" placeholder="Username" >
-            <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
-            <input type="password" name="password" id="password" placeholder="Password">
-            <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
+            <div class="Email">
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <i class="fa-solid fa-envelope fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
+            </div>
+            <div class="User">
+                <input type="text" name="Username" id="Username" placeholder="Username" >
+                <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
+            </div>
+            <div class="pass">
+                <input type="password" name="password" id="password" placeholder="Password">
+                <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
+            </div>
             <input type="checkbox" name="rememberMe" id="rememberMe">
             <span class="Rme">Remember me</span><br>
             <input type="submit" value="Login">
         </form>
-        <div class="test">
-            <span class="notHaveAnAccount">Not have an account ?</span>
-            <a href="/signUp.php">Sign up Here</a>         
+        <div class="signUpHere">
+            <span class="notHaveAnAccount">Already have an account ?</span>
+            <a href="/signUp.php">Sign in Here</a>         
         </div>
     </div>
 </body>
