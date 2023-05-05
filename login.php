8401a93df8e126c399c02ec344b5de1618f1de03
amine1949 <amine.barik.2001@gmail.com>
2023-05-05 09:35:00 +0200

    interfaces login and sign up finish     design parte

--- login.php
+++ login.php
@@ -5,22 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Include css Login -->
-    <link rel="stylesheet" href="/Glasses-Try-On/css/style.css">
-    <link rel="stylesheet" href="/Glasses-Try-On/css/login.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/login.css">
     <!-- Include css Login -->
     <!-- Inclode Icon -->
     <script src="https://kit.fontawesome.com/2f2d33a415.js" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="/Glasses-Try-On/image/glasses1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/image/glasses1.png" type="image/x-icon">
     <!-- Inclode Icon -->
     <title>GlassesVT Login</title>
 </head>
 <body>
     <div class="container">
-        <img src="/Glasses-Try-On/image/Logo.png" alt="Logo" class="logo">
+        <img src="/image/Logo.png" alt="Logo" class="logo">
         <form action="/home.php">
-            <div class="User">
-                <input type="text" name="Username" id="Username" placeholder="Username" required>
-                <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
+            <div class="mail"> 
+                <input type="email" name="Email" id="Email" placeholder="Email" required>
+                <i class="fa-solid fa-envelope fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
             </div>
             <div class="pass">
                 <input type="password" name="password" id="password" placeholder="Password" required>
@@ -32,8 +32,14 @@
         </form>
         <div class="signUpHere">
             <span class="notHaveAnAccount">Not have an account ?</span>
-            <a href="/Glasses-Try-On/signUp.php">Sign up Here</a>         
+            <a href="/signUp.php">Sign up Here</a>         
         </div>
+        <div class="socialMediaSignin">
+            <p>Sign up with social account</p>
+            <i class="fa-brands fa-google-plus-g" style="color: #d60000; font-size: 40px"></i>
+            <i class="fa-brands fa-facebook" style="color: #0d50c5; font-size: 35px"></i>
+        </div>
+
     </div>
 </body>
 </html>
--- signUp.php
+++ signUp.php
@@ -5,38 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Include css Login -->
-    <link rel="stylesheet" href="/Glasses-Try-On/css/style.css">
-    <link rel="stylesheet" href="/Glasses-Try-On/css/signUp.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/signUp.css">
     <!-- Include css Login -->
     <!-- Inclode Icon -->
     <script src="https://kit.fontawesome.com/2f2d33a415.js" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="/Glasses-Try-On/image/glasses1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="image/glasses1.png" type="image/x-icon">
     <!-- Inclode Icon -->
     <title>GlassesVT Sign Up</title>
 </head>
 <body>
     <div class="container">
-        <img src="/Glasses-Try-On/image/Logo.png" alt="Logo" class="logo">
+        <img src="image/Logo.png" alt="Logo" class="logo">
         <form action="/home.php">
             <div class="Email">
                 <input type="email" name="email" id="email" placeholder="Email" required>
                 <i class="fa-solid fa-envelope fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
             </div>
             <div class="User">
-                <input type="text" name="Username" id="Username" placeholder="Username" >
+                <input type="text" name="Username" id="Username" placeholder="Username" required>
                 <i class="fa-regular fa-user fa-fade" style="color: #19a7ce; font-size: 25px;"></i>
             </div>
             <div class="pass">
-                <input type="password" name="password" id="password" placeholder="Password">
+                <input type="password" name="password" id="password" placeholder="Password" required>
                 <i class="fa-solid fa-lock fa-beat-fade" style="color: #19a7ce; font-size: 25px;"></i><br>
             </div>
             <input type="checkbox" name="rememberMe" id="rememberMe">
             <span class="Rme">Remember me</span><br>
-            <input type="submit" value="Login">
+            <input type="submit" value="Sign up">
         </form>
+
         <div class="signUpHere">
             <span class="notHaveAnAccount">Already have an account ?</span>
-            <a href="/signUp.php">Sign in Here</a>         
+            <a href="/login.php">Sign in Here</a>         
+        </div>
+
+        <div class="socialMediaSignin">
+            <p>Sign up with social account</p>
+            <i class="fa-brands fa-google-plus-g" style="color: #d60000; font-size: 40px"></i>
+            <i class="fa-brands fa-facebook" style="color: #0d50c5; font-size: 35px"></i>
         </div>
     </div>
 </body>
